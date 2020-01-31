81e9e401b8fe6a06116575c92ae210403b00392d
My PC <mrphamdat099@gmail.com>
2020-01-31 20:49:50 +0700

    success comment for user

--- src/main/java/com/security/demospringsecurity/controller/CommentController.java
+++ src/main/java/com/security/demospringsecurity/controller/CommentController.java
@@ -3,11 +3,14 @@ package com.security.demospringsecurity.controller;
 import com.security.demospringsecurity.message.response.ResponseMessage;
 import com.security.demospringsecurity.model.Comment;
 import com.security.demospringsecurity.model.Home;
+import com.security.demospringsecurity.model.TypeHome;
+import com.security.demospringsecurity.model.TypeRoom;
 import com.security.demospringsecurity.security.service.UserPrinciple;
 import com.security.demospringsecurity.service.CommentService;
 import com.security.demospringsecurity.service.HomeService;
 import com.security.demospringsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -62,5 +65,15 @@ public class CommentController {
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editComment(@PathVariable Long id,@RequestBody Comment comment) {
+        Optional<Comment> commentCurrent = commentService.findById(id);
+        if(commentCurrent.isPresent()) {
+            commentCurrent.get().setContent(comment.getContent());
+            commentService.save(commentCurrent.get());
+            return new ResponseEntity<>(commentCurrent.get(),HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/security/demospringsecurity/model/Comment.java
+++ src/main/java/com/security/demospringsecurity/model/Comment.java
@@ -26,6 +26,4 @@ public class Comment {
     @ManyToOne
     private Home home;
 
-
-
 }
